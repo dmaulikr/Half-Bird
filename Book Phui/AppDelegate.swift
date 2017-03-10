3a570cf5c8c845a094261f58d8ae0bce969515d8
thjnh195 <thjnh195@gmail.com>
2017-03-10 23:28:59 +0700

    login screen

--- Book Phui/AppDelegate.swift
+++ Book Phui/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.configSocialNetwork()
-        self.configUI()
+//        self.configUI()
         
         return true
     }
