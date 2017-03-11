948ae57f8904f5953e1917a0eb32faf776a82adb
dungntm58 <dungntm58@vnu.edu.vn>
2017-03-11 10:46:31 +0700

    Add models

---
+++ Book Phui/Model/Stadium.swift
@@ -0,0 +1,18 @@
+//
+//  Stadium.swift
+//  Book Phui
+//
+//  Created by Thanh Tran on 3/11/17.
+//  Copyright © 2017 Half Bird. All rights reserved.
+//
+
+import Foundation
+
+class Stadium: NSObject {
+    var name: String?
+    var address: String?
+    var opennedTime: String?
+    var closedTime: String?
+    var rating: Double = 0
+    var numberOfSubYards: Int = 0
+}
---
+++ Book Phui/ViewControllers/HistoryViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HistoryViewController.swift
+//  Book Phui
+//
+//  Created by Thanh Tran on 3/11/17.
+//  Copyright © 2017 Half Bird. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var bookedStadium: [] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+}
+
